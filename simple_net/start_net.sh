61d2e2469a31884b8f5231d4eb4151a49fb548a6
adelkaiarullin <38398697+adelkaiarullin@users.noreply.github.com>
2018-04-21 15:09:50 +0300

    Update start_net.sh

--- simple_net/start_net.sh
+++ simple_net/start_net.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 proto_model_path=model_def/deploy.prototxt
-model_weights=model_def/net.caffemodel
+model_weights=model_def/net_caffemodel.caffemodel
 img=1.jpg
 ./net $proto_model_path $model_weights $img
